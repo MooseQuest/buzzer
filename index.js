c524233e20c4b089390f018e9a84cc3219297679
Kristerpher Henderson <kristerpher_henderson@comcast.com>
2020-05-02 18:41:25 -0400

    Add Passport Configuration

--- index.js
+++ index.js
@@ -5,6 +5,51 @@ const socketio = require('socket.io')
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
+// var express = require('express'); // -- called above.
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./db');
+
+// Consolidate
+
+var engines = require('consolidate');
+app.engine('pug', engines.pug);
+app.engine('ejs', engines.ejs);
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+  // Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
 
 const title = 'MQ GameShow Buzzer'
 
@@ -22,11 +67,39 @@ const getData = () => ({
 })
 
 app.use(express.static('public'))
-app.set('view engine', 'pug')
+//app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.render('index', { title }))
-app.get('/host', (req, res) => res.render('host', Object.assign({ title }, getData())))
+// app.get('/guest', (req, res) => res.render('index.pug', {title}))
 
+app.get('/guest',
+  require('connect-ensure-login').ensureLoggedIn('/login'),
+  function(req, res){
+    res.render('index.pug', { title });
+  });
+
+// app.get ('/guest') require('connect-ensure-login').ensureLoggedIn(), (req, res) => res.render('index.pug', {title})
+// app.get('/host', require('connect-ensure-login').ensureLoggedIn(), (req, res) => res.render('host.pug', Object.assign({ title }, getData())))
+
+// passport authentication
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Buzzer Application
 io.on('connection', (socket) => {
   socket.on('join', (user) => {
     data.users.add(user.id)
